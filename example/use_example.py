f1ea73c3b84554b0d0f4790b20ce1c84ecfef30a
Will Flores <waflores956@gmail.com>
2026-01-31 16:23:43 -0500

    Updating transitive dep for bazel_distribution package.

--- example/use_example.py
+++ example/use_example.py
@@ -1,7 +1,13 @@
+import argparse
 import example
 
 
 def main():
+    parser = argparse.ArgumentParser(
+                    description='PyCpp Example program',
+                    epilog='An example of how run a mix of languages with Bazel')
+    parser.parse_args()  # assign args to use later on
+
     print(f"running the example: {example.add(1, 2)}")
 
 
