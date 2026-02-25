81154c6245a3962184da1d66080e16d089592e82
Will Flores <waflores956@gmail.com>
2026-02-24 21:29:19 -0500

    updating to Bazel v9.0.0 and adding hedron compiler commands extractor.

--- example/do_read_header.cpp
+++ example/do_read_header.cpp
@@ -7,6 +7,6 @@ int main(int argc, char* argv[]) {
   // Try to import our path library
   pybind11::object path_constants = pybind11::module_::import("paths");
   auto program_path = path_constants.attr("PROGRAM_BINARY_PATH");
-  //   std::cout << program_path() << '\n';
+  std::cout << program_path() << '\n';
   return 0;
 }
