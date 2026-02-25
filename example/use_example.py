f2ec4f2f46feba1960b1a61f40752846cc16e3f2
Will Flores <waflores956@gmail.com>
2026-02-24 22:11:58 -0500

    We have a working use_example binary with bazel v9.0.0

--- example/use_example.py
+++ example/use_example.py
@@ -8,6 +8,7 @@ def main():
                     epilog='An example of how run a mix of languages with Bazel')
     parser.parse_args()  # assign args to use later on
 
+    print(dir(example))
     print(f"running the example: {example.add(1, 2)}")
 
 
